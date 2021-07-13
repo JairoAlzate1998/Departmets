9e1958ea2260571e0af76b1cbdb02879e7460ffb
Jairo Alzate <jairo.alzate@siesa.com>
2021-07-13 14:05:40 -0500

    Add function basic

--- logic.js
+++ logic.js
@@ -1,10 +1,23 @@
-let departamento = prompt("Ingrese el departamento que desees.");
-departamento = departamento.toLowerCase();
+let boton = document.getElementById("boton");
 
-switch (departamento) {
-    
 
-    default:
-        alert("Ingresa un departamento valido");
-        break;
-}
+
+const getInfo = () => {
+    let departamento = document.getElementById("departamento").value;
+    console.log(departamento);
+    switch (departamento) {
+        case "ama":
+            boton.setAttribute("href", "https://es.wikipedia.org/wiki/Leticia_(Colombia)");
+            break;
+        case "ant":
+            boton.setAttribute("href", "https://es.wikipedia.org/wiki/Medell%C3%ADn");
+            break;
+        default:
+            alert("Ingresa un departamento valido");
+            break;
+    }
+  };
+
+boton.onclick = function () {
+    getInfo();
+  };
