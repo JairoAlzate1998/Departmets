93cc4894295100fbcfcd3abb1c0223565ba98be2
Jairo Alzate <jairo.alzate@siesa.com>
2021-07-13 11:01:20 -0500

    Start project

---
+++ logic.js
@@ -0,0 +1,10 @@
+let departamento = prompt("Ingrese el departamento que desees.");
+departamento = departamento.toLowerCase();
+
+switch (departamento) {
+    
+
+    default:
+        alert("Ingresa un departamento valido");
+        break;
+}
